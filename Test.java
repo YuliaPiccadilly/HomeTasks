7ce5c437156d7d9c1779a763bfce7d9274fc6cf2
Yulia <107150984+YuliaPiccadilly@users.noreply.github.com>
2022-06-09 19:52:43 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,18 +3,16 @@ public class Test {
         Person p = new Person(); 
         p.age = 34;
         p.name = "Julia";
-       System.out.println ("Меня зовут "+p.name+". "+"мне " +p.age);
+        System.out.println ("Меня зовут "+p.name+". "+"мне " +p.age);
 
-       Person p2 = new Person(); 
-       p2.age = 3;
-       p2.name = "Lada";
-      System.out.println ("Меня зовут "+p2.name+". "+"мне " + p2.age);    
+        Person p2 = new Person(); 
+        p2.age = 3;
+        p2.name = "Lada";
+        System.out.println ("Меня зовут "+p2.name+". "+"мне " + p2.age);    
         
         p.run(); 
         p.say(); 
         p2.run();
         p2.say();
-        
-       
     }
 }
