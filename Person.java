adb268e823ba56dcacbd4896c7f741c696ca8431
Yulia <107150984+YuliaPiccadilly@users.noreply.github.com>
2022-06-09 19:50:29 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,13 +1,13 @@
-     class Person {
+    class Person {
 
     String name; 
     int age;
 
     public void run() {
-        System.out.println("My name is " + name + ". I can run");
+    System.out.println("My name is " + name + ". I can run");
     }
 
     public void say() {
-        System.out.println("My name is " + name + ". I am "+ age+" yers old." );
+    System.out.println("My name is " + name + ". I am "+ age+" yers old." );
     }
  }
