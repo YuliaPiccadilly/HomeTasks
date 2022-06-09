a1b5a209c0cdfe4dda0e2dcf330bea744b5c48f8
Yulia <107150984+YuliaPiccadilly@users.noreply.github.com>
2022-06-09 17:39:55 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,13 @@
+     class Person {
+
+    String name; 
+    int age;
+
+    public void run() {
+        System.out.println("My name is " + name + ". I can run");
+    }
+
+    public void say() {
+        System.out.println("My name is " + name + ". I am "+ age+" yers old." );
+    }
+ }
